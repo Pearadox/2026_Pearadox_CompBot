8312b3599d3eb56c11cb1a16a10291f24ea1f4d7
average-us <roger.z.cui@icloud.com>
2026-04-03 13:15:34 -0500

    made it remember its previous valid heading instead of snapping back to 0

--- src/main/java/frc/robot/util/DriveHelpers.java
+++ src/main/java/frc/robot/util/DriveHelpers.java
@@ -8,45 +8,43 @@ import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 public class DriveHelpers {
-  /**
-   * rotates the robot 45 degrees given current pose
-   *
-   * @param robotPoseSupplier
-   * @return new Rotation2d
-   */
-  public static Rotation2d findClosestCorner(Supplier<Pose2d> robotPoseSupplier) {
-    double currentDegrees = robotPoseSupplier.get().getRotation().getDegrees();
-    double[] robotCornersInDegrees = {45, 45 + 90, 45 + 180, 45 + 270};
-    double minError = Double.MAX_VALUE;
-    double closest = robotCornersInDegrees[0];
-
-    for (double c : robotCornersInDegrees) {
-
-      double error = Math.abs(Math.IEEEremainder(c - currentDegrees, 360));
-      if (error < minError) {
-        minError = error;
-        closest = c;
+    private static Rotation2d lastCourse = new Rotation2d();
+  
+    /**
+     * rotates the robot 45 degrees given current pose
+     *
+     * @param robotPoseSupplier
+     * @return new Rotation2d
+     */
+    public static Rotation2d findClosestCorner(Supplier<Pose2d> robotPoseSupplier) {
+      double currentDegrees = robotPoseSupplier.get().getRotation().getDegrees();
+      double[] robotCornersInDegrees = {45, 45 + 90, 45 + 180, 45 + 270};
+      double minError = Double.MAX_VALUE;
+      double closest = robotCornersInDegrees[0];
+  
+      for (double c : robotCornersInDegrees) {
+  
+        double error = Math.abs(Math.IEEEremainder(c - currentDegrees, 360));
+        if (error < minError) {
+          minError = error;
+          closest = c;
+        }
       }
+      return new Rotation2d(Units.degreesToRadians(closest));
     }
-    return new Rotation2d(Units.degreesToRadians(closest));
-  }
+  
+    public static Rotation2d getCourseRotation2d(
+        Supplier<ChassisSpeeds> chassisSpeedsSupplier, Supplier<Rotation2d> rotationSupplier) {
+      ChassisSpeeds chassisSpeeds =
+          ChassisSpeeds.fromRobotRelativeSpeeds(chassisSpeedsSupplier.get(), rotationSupplier.get());
+      double vx = chassisSpeeds.vxMetersPerSecond;
+      double vy = chassisSpeeds.vyMetersPerSecond;
+  
+      if (Math.hypot(vx, vy) > 0.05) {
+        double angleRad = (Math.atan2(vy, vx));
+        lastCourse = new Rotation2d(angleRad);
+      }
 
-  public static Rotation2d getCourseRotation2d(
-      Supplier<ChassisSpeeds> chassisSpeedsSupplier, Supplier<Rotation2d> rotationSupplier) {
-    ChassisSpeeds chassisSpeeds =
-        ChassisSpeeds.fromRobotRelativeSpeeds(chassisSpeedsSupplier.get(), rotationSupplier.get());
-    double vx = chassisSpeeds.vxMetersPerSecond;
-    double vy = chassisSpeeds.vyMetersPerSecond;
-
-    if (Math.hypot(vx, vy) < 0.05) {
-      return new Rotation2d();
-    }
-
-    Logger.recordOutput("Snake/vy", vy);
-    Logger.recordOutput("Snake/vx", vx);
-    double angleRad = (Math.atan2(vy, vx));
-
-    Logger.recordOutput("Snake/snake angle", Units.radiansToDegrees(angleRad));
-    return new Rotation2d(angleRad);
+      return lastCourse;
   }
 }
