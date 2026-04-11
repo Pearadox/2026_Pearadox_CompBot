2db5c59addd3cd3607fdb5d0f51268d1970abe2f
Nuhan Hasan <nmh.07000@gmail.com>
2026-04-10 20:45:33 -0500

    new luma offsets (still WiP)

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -32,9 +32,9 @@ public class VisionConstants {
   //       new Rotation3d(0, -Units.degreesToRadians(20), Math.PI));
   public static Transform3d robotToCamera1 =
       new Transform3d(
-          -Units.inchesToMeters(10.547),
-          -Units.inchesToMeters(2.928),
-          Units.inchesToMeters(14.537),
+          -Units.inchesToMeters(27.5 / 2.0 - 8),
+          -Units.inchesToMeters(27.5 / 2.0 - 3.0 / 4.0),
+          Units.inchesToMeters(13 + 7.0 / 8.0),
           new Rotation3d(0.0, 0.0, -(Math.PI * 0.5)));
 
   // Basic filtering thresholds
